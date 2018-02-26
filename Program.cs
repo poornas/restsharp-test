6c25bb3802c15c5d43746846c24fc8f19ea4d04a
poornas <k.poorna@gmail.com>
2018-02-25 23:54:13 -0700

    test project with signature mismatch issue

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net;
+using System.Collections.Generic;
+using RestSharp;
+using RestSharp.Authenticators;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace minio.netcoresampleconsoleapp
+{
+
+
+    /// <summary>
+    /// This example creates a new bucket if it does not already exist, and uploads a file
+    /// to the bucket.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var accessKey = "Q3AM3UQ867SPQQA43P2F";
+            var secretKey = "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG";
+            var client = new RestClient("https://play.minio.io:9000");
+            client.Authenticator = new V4Authenticator(true,accessKey, secretKey);
+
+            var request = new RestRequest("/", Method.GET);
+            // execute the request
+            IRestResponse response = client.Execute(request);
+            var content = response.Content; // raw content as string
+            Console.Out.WriteLine(content);
+            LogRequest(client,response.Request, response);
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Logs the request sent to server and corresponding response 
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="response"></param>
+        /// <param name="durationMs"></param>
+        private static void LogRequest(RestClient restClient, IRestRequest request, IRestResponse response)
+        {
+            var requestToLog = new
+            {
+                resource = request.Resource,
+                // Parameters are custom anonymous objects in order to have the parameter type as a nice string
+                // otherwise it will just show the enum value
+                parameters = request.Parameters.Select(parameter => new
+                {
+                    name = parameter.Name,
+                    value = parameter.Value,
+                    type = parameter.Type.ToString()
+                }),
+                // ToString() here to have the method as a nice string otherwise it will just show the enum value
+                method = request.Method.ToString(),
+                // This will generate the actual Uri used in the request
+                uri = restClient.BuildUri(request),
+            };
+
+            var responseToLog = new
+            {
+                statusCode = response.StatusCode,
+                content = response.Content,
+                headers = response.Headers,
+                // The Uri that actually responded (could be different from the requestUri if a redirection occurred)
+                responseUri = response.ResponseUri,
+                errorMessage = response.ErrorMessage,
+            };
+
+            Console.Out.WriteLine(string.Format("Request completed :- Request: {0}, Response: {1}",
+                    JsonConvert.SerializeObject(requestToLog, Formatting.Indented),
+                    JsonConvert.SerializeObject(responseToLog, Formatting.Indented)));
+        }
+
+    }
+
+
+}
---
+++ V4Authenticator.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using RestSharp;
+using RestSharp.Authenticators;
+using System.Linq;
+
+namespace minio.netcoresampleconsoleapp
+{
+        /// <summary>
+        /// V4Authenticator implements IAuthenticator interface.
+        /// </summary>
+        internal class V4Authenticator : IAuthenticator
+        {
+            private readonly string accessKey;
+            private readonly string secretKey;
+            private string Region;
+            internal bool isAnonymous { get; private set; }
+            internal bool isSecure { get; private set; }
+            //
+            // Excerpts from @lsegal - https://github.com/aws/aws-sdk-js/issues/659#issuecomment-120477258
+            //
+            //  User-Agent:
+            //
+            //      This is ignored from signing because signing this causes problems with generating pre-signed URLs
+            //      (that are executed by other agents) or when customers pass requests through proxies, which may
+            //      modify the user-agent.
+            //
+            //  Content-Length:
+            //
+            //      This is ignored from signing because generating a pre-signed URL should not provide a content-length
+            //      constraint, specifically when vending a S3 pre-signed PUT URL. The corollary to this is that when
+            //      sending regular requests (non-pre-signed), the signature contains a checksum of the body, which
+            //      implicitly validates the payload length (since changing the number of bytes would change the checksum)
+            //      and therefore this header is not valuable in the signature.
+            //
+            //  Content-Type:
+            //
+            //      Signing this header causes quite a number of problems in browser environments, where browsers
+            //      like to modify and normalize the content-type header in different ways. There is more information
+            //      on this in https://github.com/aws/aws-sdk-js/issues/244. Avoiding this field simplifies logic
+            //      and reduces the possibility of future bugs
+            //
+            //  Authorization:
+            //
+            //      Is skipped for obvious reasons
+            //
+            private static HashSet<string> ignoredHeaders = new HashSet<string>() {
+            "authorization",
+            "content-length",
+            "content-type",
+            "user-agent"
+        };
+
+            /// <summary>
+            /// Authenticator constructor.
+            /// </summary>
+            /// <param name="scheme">URL Scheme</param>
+            /// <param name="accessKey">Access key id</param>
+            /// <param name="secretKey">Secret access key</param>
+            public V4Authenticator(bool secure, string accessKey, string secretKey, string region = "us-east-1")
+            {
+                this.isSecure = secure;
+                this.accessKey = accessKey;
+                this.secretKey = secretKey;
+                this.Region = region;
+                this.isAnonymous = String.IsNullOrEmpty(accessKey) && String.IsNullOrEmpty(secretKey);
+            }
+
+
+            /// <summary>
+            /// Implements Authenticate interface method for IAuthenticator.
+            /// </summary>
+            /// <param name="client">Instantiated IRestClient object</param>
+            /// <param name="request">Instantiated IRestRequest object</param>
+            public void Authenticate(IRestClient client, IRestRequest request)
+            {
+                DateTime signingDate = DateTime.UtcNow;
+                request.AddHeader("x-amz-content-sha256", "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
+                request.AddHeader("Host", client.BaseUrl.Host + ":" + client.BaseUrl.Port);
+                request.AddHeader("x-amz-date", signingDate.ToString("yyyyMMddTHHmmssZ"));
+                var headers = request.Parameters.Where(p => p.Type.Equals(ParameterType.HttpHeader)).ToList();
+                SortedDictionary<string, string> headersToSign = new SortedDictionary<string, string>();
+                foreach (Parameter header in headers)
+                {
+                    string headerName = header.Name.ToLower();
+                    string headerValue = header.Value.ToString();
+                    headersToSign.Add(headerName, headerValue);
+                }
+                string signedHeaders = string.Join(";", headersToSign.Keys);
+                string canonicalRequest = GetCanonicalRequest(client, request, headersToSign);
+                byte[] canonicalRequestBytes = System.Text.Encoding.UTF8.GetBytes(canonicalRequest);
+                string canonicalRequestHash = BytesToHex(ComputeSha256(canonicalRequestBytes));
+                string stringToSign = GetStringToSign(this.Region, signingDate, canonicalRequestHash);
+                byte[] signingKey = GenerateSigningKey(this.Region, signingDate);
+
+                byte[] stringToSignBytes = System.Text.Encoding.UTF8.GetBytes(stringToSign);
+
+                byte[] signatureBytes = SignHmac(signingKey, stringToSignBytes);
+
+                string signature = BytesToHex(signatureBytes);
+
+                string authorization = GetAuthorizationHeader(signedHeaders, signature, signingDate, this.Region);
+                request.AddHeader("Authorization", authorization);
+
+            }
+
+            /// <summary>
+            /// Get credential string of form <ACCESSID>/date/region/s3/aws4_request. 
+            /// </summary>
+            /// <param name="signingDate">Signature initated date</param>
+            /// <param name="region">Region for the credential string</param>
+            /// <returns>Credential string for the authorization header</returns>
+            public string GetCredentialString(DateTime signingDate, string region)
+            {
+                return this.accessKey + "/" + GetScope(region, signingDate);
+            }
+
+            /// <summary>
+            /// Constructs an authorization header.
+            /// </summary>
+            /// <param name="signedHeaders">All signed http headers</param>
+            /// <param name="signature">Hexadecimally encoded computed signature</param>
+            /// <param name="signingDate">Date for signature to be signed</param>
+            /// <param name="region">Requested region</param>
+            /// <returns>Fully formed authorization header</returns>              
+            private string GetAuthorizationHeader(string signedHeaders, string signature, DateTime signingDate, string region)
+            {
+                return "AWS4-HMAC-SHA256 Credential=" + this.accessKey + "/" + GetScope(region, signingDate) +
+                    ", SignedHeaders=" + signedHeaders + ", Signature=" + signature;
+            }
+
+            /// <summary>
+            /// Generates signing key based on the region and date.
+            /// </summary>
+            /// <param name="region">Requested region</param>
+            /// <param name="signingDate">Date for signature to be signed</param>
+            /// <returns>bytes of computed hmac</returns>
+            private byte[] GenerateSigningKey(string region, DateTime signingDate)
+            {
+                byte[] formattedDateBytes = System.Text.Encoding.UTF8.GetBytes(signingDate.ToString("yyyMMdd"));
+                byte[] formattedKeyBytes = System.Text.Encoding.UTF8.GetBytes("AWS4" + this.secretKey);
+                byte[] dateKey = SignHmac(formattedKeyBytes, formattedDateBytes);
+
+                byte[] regionBytes = System.Text.Encoding.UTF8.GetBytes(region);
+                byte[] dateRegionKey = SignHmac(dateKey, regionBytes);
+
+                byte[] serviceBytes = System.Text.Encoding.UTF8.GetBytes("s3");
+                byte[] dateRegionServiceKey = SignHmac(dateRegionKey, serviceBytes);
+
+                byte[] requestBytes = System.Text.Encoding.UTF8.GetBytes("aws4_request");
+                return SignHmac(dateRegionServiceKey, requestBytes);
+            }
+
+            /// <summary>
+            /// Compute hmac of input content with key.
+            /// </summary>
+            /// <param name="key">Hmac key</param>
+            /// <param name="content">Bytes to be hmac computed</param>
+            /// <returns>Computed hmac of input content</returns>
+            private byte[] SignHmac(byte[] key, byte[] content)
+            {
+                HMACSHA256 hmac = new HMACSHA256(key);
+                hmac.Initialize();
+                return hmac.ComputeHash(content);
+            }
+
+            /// <summary>
+            /// Get string to sign.
+            /// </summary>
+            /// <param name="region">Requested region</param>
+            /// <param name="signingDate">Date for signature to be signed</param>
+            /// <param name="canonicalRequestHash">Hexadecimal encoded sha256 checksum of canonicalRequest</param>
+            /// <returns>String to sign</returns>
+            private string GetStringToSign(string region, DateTime signingDate, string canonicalRequestHash)
+            {
+                return "AWS4-HMAC-SHA256\n" +
+                    signingDate.ToString("yyyyMMddTHHmmssZ") + "\n" +
+                    GetScope(region, signingDate) + "\n" +
+                    canonicalRequestHash;
+            }
+
+            /// <summary>
+            /// Get scope.
+            /// </summary>
+            /// <param name="region">Requested region</param>
+            /// <param name="signingDate">Date for signature to be signed</param>
+            /// <returns>Scope string</returns>
+            private string GetScope(string region, DateTime signingDate)
+            {
+                string formattedDate = signingDate.ToString("yyyyMMdd");
+                return formattedDate + "/" + region + "/s3/aws4_request";
+            }
+
+            /// <summary>
+            /// Compute sha256 checksum.
+            /// </summary>
+            /// <param name="body">Bytes body</param>
+            /// <returns>Bytes of sha256 checksum</returns>
+            private byte[] ComputeSha256(byte[] body)
+            {
+
+                SHA256 sha256 = System.Security.Cryptography.SHA256.Create();
+                return sha256.ComputeHash(body);
+            }
+
+            /// <summary>
+            /// Convert bytes to hexadecimal string.
+            /// </summary>
+            /// <param name="checkSum">Bytes of any checksum</param>
+            /// <returns>Hexlified string of input bytes</returns>
+            private string BytesToHex(byte[] checkSum)
+            {
+                return BitConverter.ToString(checkSum).Replace("-", string.Empty).ToLower();
+            }
+
+
+            /// <summary>
+            /// Get canonical request.
+            /// </summary>
+            /// <param name="client">Instantiated client object</param>
+            /// <param name="request">Instantiated request object</param>
+            /// <param name="headersToSign">Dictionary of http headers to be signed</param>
+            /// <returns>Canonical Request</returns>
+            private string GetCanonicalRequest(IRestClient client, IRestRequest request,
+                SortedDictionary<string, string> headersToSign)
+            {
+                LinkedList<string> canonicalStringList = new LinkedList<string>();
+                // METHOD
+                canonicalStringList.AddLast(request.Method.ToString());
+
+                string[] path = request.Resource.Split(new char[] { '?' }, 2);
+                if (!path[0].StartsWith("/"))
+                {
+                    path[0] = "/" + path[0];
+                }
+                canonicalStringList.AddLast(path[0]);
+
+                string query = "";
+                // QUERY
+                if (path.Length == 2)
+                {
+                    var parameterString = path[1];
+                    var parameterList = parameterString.Split('&');
+                    SortedSet<string> sortedQueries = new SortedSet<string>();
+                    foreach (string individualParameterString in parameterList)
+                    {
+                        if (individualParameterString.Contains('='))
+                        {
+                            string[] splitQuery = individualParameterString.Split(new char[] { '=' }, 2);
+                            sortedQueries.Add(splitQuery[0] + "=" + splitQuery[1]);
+                        }
+                        else
+                        {
+                            sortedQueries.Add(individualParameterString + "=");
+                        }
+                    }
+                    query = string.Join("&", sortedQueries);
+                }
+                canonicalStringList.AddLast(query);
+
+                foreach (string header in headersToSign.Keys)
+                {
+                    canonicalStringList.AddLast(header + ":" + headersToSign[header]);
+                }
+                canonicalStringList.AddLast("");
+                canonicalStringList.AddLast(string.Join(";", headersToSign.Keys));
+                if (headersToSign.Keys.Contains("x-amz-content-sha256"))
+                {
+                    canonicalStringList.AddLast(headersToSign["x-amz-content-sha256"]);
+                }
+                else
+                {
+                    canonicalStringList.AddLast("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
+                }
+
+                return string.Join("\n", canonicalStringList);
+            }
+        }
+    }
